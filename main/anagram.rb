87621a345924842ef88da8d54da2811641198344
Ruslan <ruslan.n.kornienko@gmail.com>
2014-05-25 12:12:03 +0300

    Task1Works3of3

--- main/anagram.rb
+++ main/anagram.rb
@@ -2,6 +2,24 @@ module Anagram
   extend self
 
   def anagrams(str)
-    str.downcase.gsub!(/[ ,-.]/, " ").split(" ").map {|i,k| [k=i] << i.chars.sort_by(&:downcase).join}
+    arr_new = []
+    anagrams = []
+    return anagrams if str.empty?
+    str = " #{str}"
+    arr = str.downcase.gsub!(/[ ,-.]/, " ").split(" ")
+    arr_sort = arr.map {|i| i.chars.sort_by(&:downcase).join}
+    i, k = 0, 0
+    while(i != arr_sort.size)
+      while (k != arr_sort.size)
+        arr_new << arr[k] if arr_sort[i] == arr_sort[k]
+        k += 1
+      end
+      anagrams << arr_new.uniq
+      arr_new.clear
+      i += 1
+      k = i
+      break if anagrams.flatten.size == arr.size
+    end
+    anagrams
   end
 end
