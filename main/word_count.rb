2507cb31d8ca18ee58498881397836d729aeac47
Ruslan <ruslan.n.kornienko@gmail.com>
2014-05-26 15:47:34 +0300

    word_count

--- main/word_count.rb
+++ main/word_count.rb
@@ -2,7 +2,7 @@ module Words
   extend self
 
   def count(str)
-  	Hash[str.split(/\W+/).group_by(&:downcase).map {|k,v| [k, v.length]}]
+    Hash[str.split(/\W+/).group_by(&:downcase).map {|k,v| [k, v.length]}]
   end
 
 end
