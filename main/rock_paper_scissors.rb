43d2a43357ebb5c0ad7554ca691f70fdfe2d884a
Ruslan <ruslan.n.kornienko@gmail.com>
2014-05-27 12:25:31 +0300

    Task2_1

--- main/rock_paper_scissors.rb
+++ main/rock_paper_scissors.rb
@@ -1,8 +1,12 @@
 class RockPaperScissors
+  class NoSuchStrategyError < StandardError
+  end
+
   def winner(player1, player2)
+    raise NoSuchStrategyError, "Strategy must be one of R,P,S" if [player1[1], player2[1]].join('').scan(/[^SPR]/).any?
     return player1 if player1[1] == player2[1]
-    key = [player1[1], player2[1]].sort.join("_").to_sym
-    who_win = { :P_R => player2, :R_S => player1, :P_S => player2}
-    who_win[key]
+    value = [player1[1], player2[1]].join('_').to_sym
+    who_win = { :R_P => player2, :S_R => player2, :P_S => player2, :P_R => player1, :R_S => player1, :S_P => player2}
+    who_win[value]
   end
 end
